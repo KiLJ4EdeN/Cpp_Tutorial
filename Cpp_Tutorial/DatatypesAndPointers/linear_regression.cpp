f159ad2382b189880abb6edd4ba63660ca35d33e
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-15 19:22:32 +0330

    add more info to linear regression

--- Cpp_Tutorial/DatatypesAndPointers/linear_regression.cpp
+++ Cpp_Tutorial/DatatypesAndPointers/linear_regression.cpp
@@ -4,30 +4,38 @@ using namespace std;
 int main()
 {
 	// Input Features and Labels.
-	int X[5] = {1, 2, 3, 6, 10};
-	int y[5] = {1, 6, 10, 15, 30};
+	float X[5] = {1, 2, 3, 6, 10};
+	float y[5] = {2, 4, 6, 12, 20};
 
 	// y = Wx + b
-	int W[5] = {1, 3, 3, 2, 3};
-	int b[5] = {1, 1, 1, 1, 1};
+	float W = 1.0;
+	float b = 0.0;
 	
 	// Metrics
 	int y_hat, loss;
+	float learning_rate = 0.01;
 	
-	for(int i=0; i<5; i++) {
-	  y_hat = W[i] * X[i] + b[i];
-	  loss = (y[i] - y_hat);
-	  // abs
-	  if (loss >= 0) {
-		  loss = loss;
-	  }
-	  else {
-		  loss = -loss;
-	  }
-	  // If loss was not zero upgrade rule would be:
-	  // W[i] = W[i] - (0.001 * loss)
-	  cout << "Step" << i <<", Loss: " << loss << endl;
+	for(int epoch=0; epoch<10; epoch++) {
+		// Train for one epoch
+		for(int i=0; i<5; i++) {
+		  y_hat = W * X[i] + b;
+		  loss = (y[i] - y_hat);
+		  // abs
+		  if (loss >= 0) {
+			  loss = loss;
+		  }
+		  else {
+			  loss = -loss;
+		  }
+		  // If loss was not zero upgrade rule would be:
+		  // W[i] = W[i] - (0.001 * loss)
+		  W = W + (learning_rate * loss);
+		  // b = b - (learning_rate * loss);
+		  cout << "Epoch" << epoch << ", Sample" << i <<", Loss: " << loss << endl;
+		}
 	}
+	cout << "Final Weight:" << W << endl;
+	cout << "Final Bias:" << b << endl;
 	
     return 0;
 }
