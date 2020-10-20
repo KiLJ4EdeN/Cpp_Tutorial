3bf56d8de8d46520cebf0f08a4875f80925d2def
KiLJ4EdeN <parsa592323@gmail.com>
2020-10-20 16:29:42 +0330

    example

---
+++ src/Functions/Parameters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int timesTwo(int x) {
+    return x*2;
+}
+
+int main() {
+    cout << timesTwo(8) << endl;
+
+    cout <<timesTwo(5) << endl;
+
+    cout <<timesTwo(42) << endl;
+}
