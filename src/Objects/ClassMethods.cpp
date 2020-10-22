e51c100767b938a5412461fe49e576ba529fa5b2
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-22 13:09:40 +0330

    Add files via upload

---
+++ src/Objects/ClassMethods.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Dog
+{
+    public:
+        Dog();
+        void bark();
+};
+
+Dog::Dog() 
+{
+    cout << "Initiated Dog" << endl;
+}
+void Dog::bark() {
+    cout << "BAARRRK!!!!" <<endl;
+}
+
+int main() {
+    Dog obj;
+    obj.bark();
+}
