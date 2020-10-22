7dc18bdf55d1122f2bc653a6205f5d06f917d576
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-22 13:23:48 +0330

    Add files via upload

---
+++ src/Objects/Mage.cpp
@@ -0,0 +1,11 @@
+#include <Mage.h>
+#include <iostream>
+
+using namespace std;
+
+Mage::Mage(){
+}
+
+void Mage::PyroBlast(){
+	cout << "Dealt Some Damage" << endl;
+}
---
+++ src/Objects/Mage.h
@@ -0,0 +1,13 @@
+#ifndef MAGE_H
+#define MAGE_H
+
+class Mage
+{
+  public:
+	Mage();
+	void PyroBlast();
+  protected:
+  private:
+};
+
+#endif // MAGE_H 
