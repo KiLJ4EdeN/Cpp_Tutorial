24b9a6fd62e106207ba4da2f8c2ca4ab266ff5a3
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-24 13:00:49 +0330

    Update if_statement.cpp

--- src/ConditionalsAndLoops/if_statement.cpp
+++ src/ConditionalsAndLoops/if_statement.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// the if statement is good when we want to execute a block of code,
+// when a certain condition is satisfied.
+
 int main()
 {
     int a = 55;
     int b = 33;
     if (a > b) {
+        // this runs the cout part only if a is bigger than b.
         cout << "a is greater than b";
     }
 
