6f3a32d6fdfcbaaded5dbf4b021464d7d35b1f9b
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-24 10:40:09 +0330

    Update arithmetic.cpp

--- src/BasicConcepts/arithmetic.cpp
+++ src/BasicConcepts/arithmetic.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
+// lookup table for some of the available arithmetic operators in c++.
 int main() 
 {
+  // define two integers.
   int a, b;
   a = 5;
   b = 2;
-  // mathematical operations in c++.
+  // perfom some of the mathematical operations.
   cout << "Sum is: " << a + b << endl;
   cout << "Divison: " << a / b << endl;
   cout << "Reminder:" << a % b << endl;
   cout << "Mult": << a * b << endl;
+  // pay attention to the order of operations.
+  // (), */, +-
   cout << "Operation Orders": << (a + b) + 10 / 2 << endl;
 
 
