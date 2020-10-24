c4e3531274c5a612577f143dc846dc504c8a9a84
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-24 12:59:48 +0330

    Update for_statement.cpp

--- src/ConditionalsAndLoops/for_statement.cpp
+++ src/ConditionalsAndLoops/for_statement.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// the for loop lets us execute a block of code for consequtive times.
+
+
 int main()
 {
     for (int a = 0; a < 50; a+=10) {
+        // note that the loop condition can also be used for indexing arrays, etc..
         cout << a << endl;
     }
     return 0;
