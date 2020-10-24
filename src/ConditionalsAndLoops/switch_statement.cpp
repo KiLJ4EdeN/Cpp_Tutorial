42d4ce00310cda62f290bf0437d7cc8e3ff2bd66
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-24 13:04:58 +0330

    Update switch_statement.cpp

--- src/ConditionalsAndLoops/switch_statement.cpp
+++ src/ConditionalsAndLoops/switch_statement.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// switch releaves us from writing if else if loops, 
+// by targeting a variable and doing different jobs based on the input received.
+
 int main()
 {
     int age = 25;
     switch (age) {
+        // for different cases, we execute different blocks,
         case 16:
             cout << "Too young";
             break;
@@ -14,6 +18,7 @@ int main()
         case 70:
             cout << "Senior";
             break;
+        // there is also a default case for when none of conditions are provided.
         default:
             cout << "This is the default case";
     }
