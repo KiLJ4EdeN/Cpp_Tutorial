db1d232d0805181753ea0b51b78b1ffe44c5854d
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-24 12:58:21 +0330

    Update else_statement.cpp

--- src/ConditionalsAndLoops/else_statement.cpp
+++ src/ConditionalsAndLoops/else_statement.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// else provides us with a way do something, when a if condition is not satisfied.
 int main()
 {
     int age = 18;
+    // first if condition.
     if (age > 14) {
+	// second if condition.
         if(age >= 18) {
             cout << "Adult";
         }
+	// age in range 14 to 18.
         else {
             cout << "Teenager";
         }
     }
+    // age is less than 14
     else {
         if (age > 0) {
             cout << "Child";
@@ -20,8 +25,9 @@ int main()
             cout << "Something's wrong";
         }
     }
-    /* 
-	int a = 10;
+    /*
+    // important to note that you dont need curly braces if the loop body is short enough.
+    int a = 10;
     if (a > 4)
         cout << "Yes";
     else
