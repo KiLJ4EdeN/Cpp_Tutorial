b76298a592ae7e4996edefcf5df5c229522abe1b
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-24 10:37:20 +0330

    Update Calculator.cpp

--- src/BasicConcepts/Calculator.cpp
+++ src/BasicConcepts/Calculator.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 
 int main(){
+	// declare two integers.
 	int num1, num2;
+	// declare operation type as a character.
 	char op;
+	// get the numbers and the operation from the user and store them in respective variables.
 	cout << "Enter The First Number: " << endl;
 	cin >> num1;
 	cout << "Enter +, -, *, /" << endl;
@@ -12,7 +15,8 @@ int main(){
 	cout << "Enter The Second Number: " << endl;
 	cin >> num2;
 	cout << "" << endl;
-
+	
+	// find the requested operation and print an error massage if the operation is not in the list.
 	if (op == '+') {
 		cout << num1 + num2 << endl;
 	} else if (op == '-') {
