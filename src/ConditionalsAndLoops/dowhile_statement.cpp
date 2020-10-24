49b90defbd54356bfff819e3c45416d588001beb
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-24 12:55:06 +0330

    Update dowhile_statement.cpp

--- src/ConditionalsAndLoops/dowhile_statement.cpp
+++ src/ConditionalsAndLoops/dowhile_statement.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// we ll go over the do while loop in this one.
+// the do while loop is different from a noraml while loop in a sense that, a do while loop at least runs once.
+
 int main()
-{
+{   
+    
     int a = 42;
+    // do the operation once. 
     do {
         cout << a << endl;
         a++;
+    // check the condition to continue.
     } while(a < 5);
 
     return 0;
