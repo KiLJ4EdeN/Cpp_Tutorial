99e261a06e77b31656766373cd2c8f7b8ca2e8ce
KiLJ4EdeN <parsa592323@gmail.com>
2020-10-28 08:27:42 +0330

    more imageprocessing

---
+++ src/ComputerVision/OpenCV/ImageProcessing/DisplayImage.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+
+int main(){
+	// Resizing Metrics.
+	float fx = 0.5;
+	float fy = 0.5;
+	int t = 0;
+
+	// Save the image path in a variable.
+	std::string image_path = "OpenCV.png";
+
+	// Window name required by opencv.
+	std::string window_name = "Image";
+
+	// Mat container for the image.
+	cv::Mat image;
+
+	// Load the image in color mode.
+	image = cv::imread(image_path, cv::IMREAD_COLOR);
+	
+	// Resize The image if needed.
+	cv::resize(image, image, cv::Size(t, t), fx=fx, fy=fy);
+
+	// Show the image and wait for a key to be pressed.
+	cv::imshow(window_name, image);
+	cv::waitKey(0);
+
+	// Destroy the active window afterwards.
+	cv::destroyAllWindows();
+
+	// Return 0 if everything went right.
+	return 0;
+}
---
+++ src/ComputerVision/OpenCV/ImageProcessing/GrayCamera.cpp
@@ -0,0 +1,47 @@
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+#include <iostream>		// standard C++ I/O
+#include <string>		// standard C++ dtypes
+#include <algorithm>    	// includes max()
+
+
+int main()
+{
+	cv::Mat img;  // image object
+  	const std::string windowName = "OpenCV Camera"; // window name
+
+  	// create window object
+  	cv::namedWindow(windowName, 1);
+
+  	// grab an image from camera (here assume only 1 camera, device 0)
+  	cv::VideoCapture cap(0);  // video capture object
+
+  	if( !cap.isOpened() ){
+    	std::cout << "error: could not grab a frame" << std::endl;
+    	exit(0);
+ 	 }
+ 	 // start the stream if everything is set.
+
+  	while ( true ){
+  		cap >> img; // retrieve the captured frame as an image
+		
+		// convert image to grayscale.
+		cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+
+  		// display image in window
+  		cv::imshow(windowName, img);
+
+  		// start event processing loop (very important,in fact essential for GUI)
+  		int key = ( cv::waitKey(1) & 0xFF );
+  		if ( key == 'q' ){
+			break;
+		}
+
+  		// the camera will be deinitialized automatically in VideoCapture destructor
+  		// all OK : main returns 0
+  		}
+  	return 0;
+
+}
